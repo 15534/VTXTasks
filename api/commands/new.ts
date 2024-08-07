0c3c0276556771080f851c2292a846bb7c0077fe
Owen Wood <owenwood0101@icloud.com>
2024-08-07 17:42:06 -0400

    Update new.ts

--- api/commands/new.ts
+++ api/commands/new.ts
@@ -34,7 +34,7 @@ export const data = {
       custom_id: "assignees",
       description: "Members responsible for the task",
       required: true,
-      max_values: 15,
+      max_length: 15,
     },
     {
       type: MessageComponentTypes.STRING,
