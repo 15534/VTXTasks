be081e6f24cac96b48feac749e906678d3e30a56
Owen Wood <owenwood0101@icloud.com>
2024-08-07 15:08:04 -0400

    test

--- api/util.ts
+++ api/util.ts
@@ -1,7 +1,7 @@
 export enum MessageComponentTypes {
   STRING_SELECT = 3,
-  INPUT_TEXT = 2,
-  MENTIONABLE_SELECT = 2,
+  INPUT_TEXT = 6,
+  MENTIONABLE_SELECT = 9,
 }
 
 export enum TextInputStyles {
