0c0155b3e0484b8d981279d00c436573e0f4f32b
Owen Wood <owenwood0101@icloud.com>
2024-08-06 22:55:31 -0400

    feat(new): multiple components per message

--- api/commands/new.ts
+++ api/commands/new.ts
@@ -31,11 +31,26 @@ export const getResponse = () => {
                 }
               ]
             },
+          ],
+        },
+        {
+          type: 1,
+          components: [
             {
-              type: MessageComponentTypes.ROLE_SELECT,
-              label: 'fdsa',
-              custom_id: 'gr-input',
-              placeholder: 'fdsa Input',
+              type: MessageComponentTypes.STRING_SELECT,
+              label: 'Short Input',
+              custom_id: 'fds-input',
+              placeholder: 'Short Input',
+              options: [
+                {
+                  label: 'Option 1',
+                  value: 'option1',
+                },
+                {
+                  label: 'Option 2',
+                  value: 'option2',
+                }
+              ]
             },
           ],
         },
