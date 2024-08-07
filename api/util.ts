03f753e8182f9d8d7bfe64e4b3bbc49cd1c249a2
Owen Wood <owenwood0101@icloud.com>
2024-08-07 14:57:00 -0400

    Update util.ts

--- api/util.ts
+++ api/util.ts
@@ -1,12 +1,7 @@
 export enum MessageComponentTypes {
-  ACTION_ROW = 1,
-  BUTTON = 2,
   STRING_SELECT = 3,
-  INPUT_TEXT = 4,
-  USER_SELECT = 5,
-  ROLE_SELECT = 6,
+  INPUT_TEXT = 10,
   MENTIONABLE_SELECT = 9,
-  CHANNEL_SELECT = 8,
 }
 
 export enum TextInputStyles {
