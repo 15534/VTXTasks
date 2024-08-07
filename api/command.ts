2ae0eeebfc339d8494b2270c51245e1fa8792cc8
Owen Wood <owenwood0101@icloud.com>
2024-08-06 21:24:39 -0400

    test(new): modal test

--- api/command.ts
+++
@@ -1,8 +0,0 @@
-import { InteractionResponseType } from 'discord-interactions';
-
-export interface ResponseType {
-  type: InteractionResponseType,
-  data: {
-    content: string,
-  }
-}
--- api/commands/new.ts
+++ api/commands/new.ts
@@ -1,18 +1,43 @@
 import { InteractionResponseType } from 'discord-interactions';
-import { ResponseType } from '../command';
 
 export const data = {
   name: 'new',
   description: "Create a new ticket",
 };
 
-export const getResponse = (): ResponseType => {
-
-
+export const getResponse = () => {
   return {
-    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    type: InteractionResponseType.MODAL,
     data: {
-      content: 'Working!',
+      title: 'Test',
+      custom_id: 'test-modal',
+      components: [
+        {
+          type: 1,
+          components: [
+            {
+              type: 4,
+              style: 1,
+              label: 'Short Input',
+              custom_id: 'short-input',
+              placeholder: 'Short Input',
+            },
+          ],
+        },
+        {
+          type: 1,
+          components: [
+            {
+              type: 4,
+              style: 1,
+              label: 'Paragraph Input',
+              custom_id: 'paragraph-input',
+              placeholder: 'Paragraph Input',
+              required: false,
+            },
+          ],
+        },
+      ],
     },
-  }
+  };
 };
