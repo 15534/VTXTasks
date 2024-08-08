fff451d33b923c75dc862722cbf407728700089a
Owen Wood <owenwood0101@icloud.com>
2024-08-08 01:46:28 -0400

    Update index.ts

--- api/commands/index.ts
+++ api/commands/index.ts
@@ -1,8 +1,11 @@
 import * as create from './new';
 import * as register from "./register"
 import * as deregister from "./deregister"
+import * as status from "./status"
+
 export default {
   new: create,
   register,
-  deregister
+  deregister,
+  status
 };
