b167d3c21d3c76133f5dd102735e0d3ef2c6588b
Owen Wood <owenwood0101@icloud.com>
2024-08-08 00:28:06 -0400

    Update new.ts

--- api/commands/new.ts
+++ api/commands/new.ts
@@ -193,24 +193,24 @@ export const getResponse = async (message) => {
           title,
           description,
           color: 0xFF0000,
-          // timestamp: new Date().toISOString(),
-          // fields: [
-          //   {
-          //     name: 'Type',
-          //     value: type.split('')[0].toUpperCase() + type.slice(1),
-          //     inline: true,
-          //   },
-          //   {
-          //     name: 'Subgroup',
-          //     value: subgroup.split('')[0].toUpperCase() + subgroup.slice(1),
-          //     inline: true,
-          //   },
-          //   {
-          //     name: 'Priority',
-          //     value: priority.split('')[0].toUpperCase() + priority.slice(1),
-          //     inline: true,
-          //   },
-          // ],
+          timestamp: new Date().toISOString(),
+          fields: [
+            {
+              name: 'Type',
+              value: type.split('')[0].toUpperCase() + type.slice(1),
+              inline: true,
+            },
+            {
+              name: 'Subgroup',
+              value: subgroup.split('')[0].toUpperCase() + subgroup.slice(1),
+              inline: true,
+            },
+            {
+              name: 'Priority',
+              value: priority.split('')[0].toUpperCase() + priority.slice(1),
+              inline: true,
+            },
+          ],
         }
       ]
     })
